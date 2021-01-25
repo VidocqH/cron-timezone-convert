fd3954d89b5466727c5ca59b8163028d8bed711d
Vidocq <949867756@qq.com>
2021-01-26 00:04:33 +0800

    update

--- README.md
+++ README.md
@@ -15,9 +15,3 @@ convert(cron_expression, source_timezone, target_timezone)
 ### TODO
 
 Change day and week when switch timezone
-
-### Why I do this?
-
-I find the cron of Github actions is at the UTC timezone and I have a lot of actions need to maintain their start time. So I write this.
-
-It can only change hour now. Need to fix.
--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="crontztrans", # Replace with your own username
+    name="crontzconvert", # Replace with your own username
     version="0.0.1",
     author="Vidocq",
     author_email="vidocqho@gmail.com",
     description="Change crontab between two timezones.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/vidocqh/crontzconvert",
+    url="https://github.com/vidocqh/cron-timezone-convert",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
