c993846f1ad65bde0faf7571ff5dbd5cd2f41843
Vidocq <949867756@qq.com>
2021-01-26 00:30:10 +0800

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="crontzconvert", # Replace with your own username
-    version="0.0.1",
+    version="0.1.0",
     author="Vidocq",
     author_email="vidocqho@gmail.com",
     description="Change crontab between two timezones.",
